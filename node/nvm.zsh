aa6f6bbd29488073683e06658218a16424e49c50
Trevor Dorsey <tsdorsey@gmail.com>
2016-08-16 01:12:03 -0700

    Add nvm config

---
+++ node/nvm.zsh
@@ -0,0 +1,20 @@
+NVM_DIR="/Users/`whoami`/.nvm"
+
+if [[ -d "$NVM_DIR" ]]; then
+  # This loads nvm
+  export NVM_DIR
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+  # place this after nvm initialization!
+  autoload -U add-zsh-hook
+  load-nvmrc() {
+    if [[ -f .nvmrc && -r .nvmrc ]]; then
+      nvm use
+    elif [[ $(nvm version) != $(nvm version default)  ]]; then
+      echo "Reverting to nvm default version"
+      nvm use default
+    fi
+  }
+  add-zsh-hook chpwd load-nvmrc
+  load-nvmrc
+fi
