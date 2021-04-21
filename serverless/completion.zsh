ee7faef717150f919bc5e0e584e71cf192c77004
Trevor Dorsey <trevor@foyyay.com>
2021-04-21 10:23:45 -0700

    Use dotfile completion pattern for serverless tab completion

---
+++ serverless/completion.zsh
@@ -0,0 +1,12 @@
+
+# tabtab source for serverless package
+# uninstall by removing these lines
+[[ -f ./serverless.zsh ]] && . ./serverless.zsh || true
+
+# tabtab source for sls package
+# uninstall by removing these lines
+[[ -f ./sls.zsh ]] && . ./sls.zsh || true
+
+# tabtab source for slss package
+# uninstall by removing these lines
+[[ -f ./slss.zsh ]] && . ./slss.zsh || true
